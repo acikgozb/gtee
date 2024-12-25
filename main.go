2440d70408e99eed00c51f9ef1f947e497e3d011
acikgozb <berkacikgoz@armut.com>
2024-12-26 00:32:56 +0300

    [main] Implement base usage (no flag, no file)
    By default, `tee` copies stdin to stdout if no files are given.
    Therefore, the initial step is implemented in the same way.
    
    Also, a placeholder signal notification is implemented for SIGINT.
    The implementation is the same as how Go handles SIGINT by default.
    Based on needs, the implementation may be removed in future.

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+)
 
 func main() {
-	fmt.Println("[main] init repository")
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	go func(ctx context.Context) {
+		<-ctx.Done()
+		stop()
+
+		fmt.Printf("gtee: sig %s\n", os.Interrupt)
+		os.Exit(1)
+	}(ctx)
+
+	run()
+}
+
+func run() {
+	if _, err := io.Copy(os.Stdout, os.Stdin); err != nil {
+		log.Fatal(err)
+	}
 }
