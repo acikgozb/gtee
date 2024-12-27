f3e647d7ca06b8493a8114e5cfa72061d4fa740a
acikgozb <berkacikgoz@armut.com>
2024-12-27 15:01:30 +0300

    [flags] Implement buffered stdin
    In order to read big inputs efficiently, stdin is read with buffering
    via bufio.Scanner.
    
    By default, bufio.Scanner reads bytes one by one via its bufio.ScanBytes
    function.
    Therefore, a custom bufio.SplitFunc is implemented to read 64k bytes at
    most.
    
    Read process is separated from the main goroutine in order to process
    writes concurrently.

--- main.go
+++ main.go
@@ -15,32 +15,15 @@ type config struct {
 	append bool
 }
 
+var (
+	name          = "gtee"
+	readBufSize   = bufio.MaxScanTokenSize
+	ignoredSIGINT = fmt.Sprintf("%s: %s", name, "The SIGINT signal is ignored.")
+)
+
 func (c *config) parse() {
-	flag.BoolVar(&c.ignore, "i", c.ignore, "Ignore SIGINT")
+	flag.BoolVar(&c.ignore, "i", c.ignore, "Ignore the SIGINT signal.")
 	flag.Parse()
-}
-
-func ignoreSIGINT() {
-	c := make(chan os.Signal, 2)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-
-	go func() {
-		for {
-			switch s := <-c; s {
-			case os.Interrupt:
-				fmt.Println("\ngtee: SIGINT is suppressed.")
-			case syscall.SIGTERM:
-				close(c)
-				os.Exit(1)
-			}
-		}
-	}()
-}
-
-func gtee() {
-	if _, err := io.Copy(os.Stdout, os.Stdin); err != nil {
-		log.Fatal(err)
-	}
 }
 
 func main() {
@@ -54,5 +37,87 @@ func main() {
 		ignoreSIGINT()
 	}
 
-	gtee()
+	gtee(&cfg)
+}
+
+func ignoreSIGINT() {
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		for {
+			switch s := <-c; s {
+			case os.Interrupt:
+				fmt.Println(ignoredSIGINT)
+			case syscall.SIGTERM:
+				close(c)
+				os.Exit(1)
+			}
+		}
+	}()
+}
+
+func gtee(cfg *config) {
+	fnames := flag.Args()
+	fnames = append(fnames, os.Stdout.Name())
+
+
+	var wg sync.WaitGroup
+	rc := make(chan []byte)
+	ec := make(chan error)
+
+	for range fnames {
+		wg.Add(1)
+		go func(rc <-chan []byte, wg *sync.WaitGroup) {
+			for b := range rc {
+				fmt.Printf("Got chunk: %d", len(b))
+			}
+			wg.Done()
+		}(rc, &wg)
+
+	}
+
+	wg.Add(1)
+	go readStdin(rc, ec, &wg)
+
+	wg.Wait()
+
+}
+
+func main() {
+	cfg := config{
+		ignore: false,
+		append: false,
+	}
+	cfg.parse()
+func readStdin(rc chan<- []byte, ec chan<- error, wg *sync.WaitGroup) {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		if atEOF {
+			// Data may not be empty: https://pkg.go.dev/bufio#SplitFunc
+			return 0, data, bufio.ErrFinalToken
+		}
+
+		if len(data) > readBufSize {
+			return readBufSize, data[:readBufSize], nil
+		}
+
+		if len(data) <= readBufSize {
+			return len(data), data, nil
+		}
+
+		return 0, nil, nil
+	})
+
+	for sc.Scan() {
+		b := sc.Bytes()
+		rc <- b
+	}
+
+	err := sc.Err()
+	if err != nil {
+		ec <- err
+	}
+	close(rc)
+	wg.Done()
 }
